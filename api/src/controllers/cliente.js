f641e5c12068ec8a1ad1ba80b7c3c56e2641eb24
thamyeS <sthamyecorrea@gmail.com>
2025-04-15 16:08:05 -0300

    teste

---
+++ api/src/controllers/cliente.js
@@ -0,0 +1,71 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const create = async (req, res) => {
+    try {
+        const cliente = await prisma.cliente.create({
+            data: req.body
+        });
+        res.status(201).json(cliente).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
+}
+
+const read = async (req, res) => {
+    const clientes = await prisma.cliente.findMany();
+    res.json(clientes);
+}
+
+const readOne = async (req, res) => {
+    try {
+        const cliente = await prisma.cliente.findUnique({
+            select: {
+                telefone: true,
+                pedido: true,
+            },
+            where: {
+                id: req.params.id
+            }
+        });
+        return res.json(cliente);
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+
+const update = async (req, res) => {
+    try {
+        const cliente = await prisma.cliente.update({
+            data: req.body,
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(202).json(cliente).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const cliente = await prisma.cliente.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(204).json(cliente).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    readOne,
+    update,
+    remove
+}
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const routes = express.Router();
 
+const Cliente = require('./controllers/cliente.js');
+const Telefone = require('./controllers/telefone.js');
+const Pedido = require('./controllers/pedido.js');
+
 routes.get('/', (req, res) => {
   return res.json({ titulo: 'SNOOPY PetShop' });
 });
+
+routes.post('/clientes',Cliente.create);
+routes.get('/clientes',Cliente.read);
+routes.get('/clientes/:id',Cliente.readOne);
+routes.patch('/clientes/:id',Cliente.update);
+routes.delete('/clientes/:id',Cliente.remove);
+
+routes.post('/telefones',Telefone.create);
+routes.get('/telefones',Telefone.read);
+routes.get('/telefones/:id',Telefone.readOne);
+routes.patch('/telefones/:id',Telefone.update);
+routes.delete('/telefones/:id',Telefone.remove);
+
+routes.post('/pedidos',Pedido.create);
+routes.get('/pedidos',Pedido.read);
+routes.get('/pedidos/:id',Pedido.readOne);
+routes.patch('/pedidos/:id',Pedido.update);
+routes.delete('/pedidos/:id',Pedido.remove);
 
 module.exports = routes;
